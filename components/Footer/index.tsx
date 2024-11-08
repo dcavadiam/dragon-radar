2325b50953a497fdaee7c0fee13e499627281773
Diego <dcavadia@uninorte.edu.co>
2024-11-08 13:23:38 -0500

    fix: header and footer text

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,7 +2,7 @@
 export const Footer = () => {
     return (
         <footer className="flex justify-center items-center w-full py-3 px-6 border-t border-gray-800">
-            Made with ❤️ by <a href="https://github.com/dcavadiam">Diego Cavadia</a>
+            <span>Made with ❤️ by <a href="https://github.com/dcavadiam">Diego Cavadia</a></span>
         </footer>
     )
 }
